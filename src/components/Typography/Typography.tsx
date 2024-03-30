337cd516e56a79defbc26059a0f87686df022bcc
ribgu <gustavo.ribeiro@sptech.school>
2024-03-30 00:50:53 -0300

    add typography variants

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,5 +1,5 @@
 type TypographyProps = {
-    variant: 'h1' | 'h2' | 'h3' | 'body1' | 'body2'
+    variant: 'h1' | 'h2' | 'h3' | 'body1' | 'body2' | 'lp-title' | 'lp-card-title'
     children: React.ReactNode
 }
 
@@ -7,12 +7,15 @@ export default function Typography(props: TypographyProps) {
     const { variant, children } = props
 
     const variantsClasnames = {
-        h1: 'text-3xl font-bold',
-        h2: 'text-2xl font-bold',
-        h3: 'text-xl font-bold',
-        body1: 'text-base font-normal',
-        body2: 'text-sm font-normal'
+        'lp-title': 'font-montserrat text-8xl font-light', // 96px
+        'lp-card-title': 'font-manrope text-4xl font-bold', // 36px
+        h1: 'font-montserrat text-5xl font-bold', // 48px
+        h2: 'font-montserrat text-4xl font-bold', // 36px
+        h3: 'font-montserrat text-2xl font-bold', // 24px
+        body1: 'font-montserrat text-base font-normal', // 16px
+        body2: 'font-montserrat text-sm font-normal' // 14px
     }
+
     const className = variantsClasnames[variant]
 
     return (
