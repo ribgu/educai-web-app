4ebddf544f0e5c681877dc6929e0263d68e21498
Erick Nunes De Araujo <enaraujo@stefanini.com>
2024-04-03 20:56:27 -0300

    Correção uso de componentes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,31 +1,33 @@
 import { SlSocialFacebook, SlSocialInstagram, SlSocialTwitter } from "react-icons/sl";
-
+import Typography from "../Typography/Typography";
+import Logo from "../Logo/Logo";
 
 
 export default function Footer() {
     return (
-        <div className="flex flex-col justify-between min-h-[120px] relative px-11 py-2 bg-dark">
-            <div className="flex justify-between px-11 grid-span-1">
-                <div className="flex align-middle">
-                    <figure className="max-h-[40px] max-w-[40px]"><img src="../../public/logos/bookTwo.svg" alt="livrinho" /></figure>
-                    <h1 className="ml-4 text-[32px] text-white text-center">educAI</h1>
+        <div className="flex flex-col justify-between min-h-[100px] relative px-11 py-2 bg-dark">
+            <div className="flex justify-between items-center px-11 h-[58%]">
+                <div className="flex items-center justify-center w-[148px]">
+                    <Logo variant="bookTwo" width="lg" />
+                    <Typography variant="h3">educAI</Typography>
                 </div>
 
-                <div className="flex justify-between w-[220px]">
-                    <button className="bg-transparent border border-white rounded-full p-4 hover:bg-white hover:border-transparent transition duration-300">
-                        <SlSocialFacebook size={20} />
+                <div className="flex justify-between w-[180px]">
+                    <button className="btn btn-circle btn-outline">
+                        <SlSocialFacebook/>
                     </button>
-                    <button className="bg-transparent border border-white rounded-full p-4 hover:bg-white hover:border-transparent transition duration-300">
-                        <SlSocialInstagram size={20} />
+                    <button className="btn btn-circle btn-outline">
+                        <SlSocialInstagram />
                     </button>
-                    <button className="bg-transparent border border-white rounded-full p-4 hover:bg-white hover:border-transparent transition duration-300">
-                        <SlSocialTwitter size={20} />
+                    <button className="btn btn-circle btn-outline">
+                        <SlSocialTwitter />
                     </button>
+                    
                 </div>
             </div>
 
-            <div className="flex justify-center after:content-[''] after:absolute after:w-[88%] after:h-[1px] after:bg-slate-700">
-                <p className="text-sm mt-4">@ 2024 Copyright by EducAI</p>
+            <div className="flex items-bottom justify-center pt-2 h-[42%] after:content-[''] after:absolute after:w-[88%] after:h-[1px] after:bg-slate-700 after:mb-4">
+                <Typography variant="body2">@2024 Copyright by educAI</Typography>
             </div>
         </div>
     )
