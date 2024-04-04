4457303cd5a900fa649db1d95e77657c8904807b
ribgu <gustavo.ribeiro@sptech.school>
2024-04-03 21:18:09 -0300

    cria componente de item pro drawer

--- src/components/Drawer/components/DrawerItem.tsx
+++ src/components/Drawer/components/DrawerItem.tsx
@@ -1,25 +1,62 @@
 import ListItem from '@mui/material/ListItem'
+import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 
 type ItemProps = {
   name: string
   icon: string
   path: string
+  variant: 'selected' | 'unselected'
 }
 export default function DrawerItem(props: ItemProps) {
-  const { name, icon } = props
+  const { name, icon, variant } = props
   return (
-    <ListItem sx={{
-      display: 'flex',
-      gap: '10px',
-      backgroundColor: 'white',
-      width: '128px',
-      padding: '10px'
-    }}>
-      <img src={icon} alt={name} />
-      <Typography variant='h6' sx={{
-        fontWeight: 'bold'
-      }}>{name}</Typography>
-    </ListItem>
+    <>
+      {variant === 'selected' && (
+        <ListItem sx={{
+          display: 'flex',
+          gap: '10px',
+          backgroundColor: '#F1EBFF',
+          justifyContent: 'center',
+          borderRadius: '0 24px 24px 0',
+          width: '220px',
+          padding: '14px'
+        }}>
+          <Stack sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '10px',
+            flexDirection: 'row'
+          }}  >
+            <img src={icon} alt={name} />
+            <Typography variant='h6' sx={{
+              fontWeight: 'bold'
+            }}>{name}</Typography>
+          </Stack>
+        </ListItem>
+      )}
+
+      {variant === 'unselected' && (
+        <ListItem sx={{
+          display: 'flex',
+          gap: '10px',
+          justifyContent: 'center',
+          width: '220px',
+          padding: '14px'
+        }}>
+          <Stack sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '10px',
+            flexDirection: 'row'
+          }}  >
+            <img src={icon} alt={name} />
+            <Typography variant='h6' sx={{
+              fontWeight: 'bold'
+            }}>{name}</Typography>
+          </Stack>
+        </ListItem>
+      )}
+    </>
   )
 }
