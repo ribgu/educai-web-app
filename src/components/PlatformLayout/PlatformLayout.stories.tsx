f605a66e455b61960c932c12f58b05d82d205d08
ribgu <gustavo.ribeiro@sptech.school>
2024-04-03 21:18:24 -0300

    cria componente de layout padrao

---
+++ src/components/PlatformLayout/PlatformLayout.stories.tsx
@@ -0,0 +1,15 @@
+import TopBar from '../TopBar/TopBar'
+import Drawer from '../Drawer/SideDrawer'
+
+export default {
+  title: 'Components/PlatformLayout'
+}
+
+export const PlatformLayoutStories = () => {
+  return (
+    <>
+      <TopBar />
+      <Drawer />
+    </>
+  )
+}
---
+++ src/components/PlatformLayout/PlatformLayout.tsx
@@ -0,0 +1,11 @@
+import Drawer from '@mui/material/Drawer'
+import TopBar from '../TopBar/TopBar'
+
+export default function PlatformLayout() {
+  return(
+    <>
+      <TopBar />
+      <Drawer />
+    </>
+  )
+}
