b3d1419367bc98b6934270dda73edc34d851dd43
ribgu <gustavo.ribeiro@sptech.school>
2024-04-15 21:38:54 -0300

    começa accordion

--- src/components/Drawer/components/DrawerItem.tsx
+++ src/components/Drawer/components/DrawerItem.tsx
@@ -1,12 +1,15 @@
 import ListItem from '@mui/material/ListItem'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 type ItemProps = {
   name: string
   icon: string
   path: string
-  variant: 'selected' | 'unselected'
+  variant: 'selected' | 'unselected' | 'turma-selected' | 'turma-unselected'
   color?: string
 }
 export default function DrawerItem(props: ItemProps) {
@@ -61,6 +64,36 @@ export default function DrawerItem(props: ItemProps) {
             }}>{name}</Typography>
           </Stack>
         </ListItem>
+      )}
+
+      {variant === 'turma-selected' && (
+        <Accordion sx={{
+          display: 'flex',
+          backgroundColor: '#F1EBFF',
+          justifyContent: 'center',
+          borderRadius: '0 24px 24px 0',
+          width: '220px'
+        }}>
+          <Stack sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '10px',
+            flexDirection: 'row'
+          }}  >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls='panel1-content'
+              id='panel1-header'
+            >
+              <img src={icon} alt={name} />
+              <Typography variant='body1' sx={{
+                fontWeight: 'bold',
+                color: colorHex
+              }}>{name}</Typography>
+            </AccordionSummary>
+
+          </Stack>
+        </Accordion>
       )}
     </>
   )
