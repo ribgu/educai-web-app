1041673e7171ed038d9eabd4c979340f1411e892
ribgu <gustavo.ribeiro@sptech.school>
2024-04-15 21:05:06 -0300

    add logo

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -2,7 +2,7 @@ import Stack from '@mui/material/Stack'
 import Logo from '../Logo/Logo'
 import ProfileHeader from '../ProfileHeader/ProfileHeader'
 
-export default function TopBar() {
+export default function   TopBar() {
   return (
     <Stack width='100%' height='80px' sx={{
       borderBottom: '1px solid transparent',
@@ -17,7 +17,7 @@ export default function TopBar() {
       padding: '20px'
     }}>
       {/* adicionar variante certa  */}
-      <Logo variant='padraoWhite' width='md' />
+      <Logo variant='padraoWhite' width='sm' />
       <ProfileHeader name='Gustavinho' imageUrl='https://avatars.githubusercontent.com/u/6713782?v=4' />
     </Stack>
   )
