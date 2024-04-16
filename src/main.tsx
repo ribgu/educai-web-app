fd2efc9d0012f51881a263445f015fff5feaef90
ribgu <gustavo.ribeiro@sptech.school>
2024-04-15 21:05:13 -0300

    add rota teste

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import LandingPage from './pages/LandingPage.tsx'
+import PlatformLayout from './components/PlatformLayout/PlatformLayout.tsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+  },
+  {
+    path: '/teste',
+    element: <PlatformLayout />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
