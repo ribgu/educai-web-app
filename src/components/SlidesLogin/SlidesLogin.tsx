2d20a9ee951eafb56ad3ffc930acb6b587085ad7
juliaRufino <julia.rufino@sptech.school>
2024-04-20 16:00:59 -0300

    create Slide Login component

---
+++ src/components/SlidesLogin/SlidesLogin.tsx
@@ -0,0 +1,48 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import '../../App.css';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Typography from '../Typography/Typography'
+
+export default function SlideLogin() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination, Navigation]}
+        className='mySwiper swiper-login'
+      >
+        <SwiperSlide className='swiper-slide-login'>
+          <img src="../../../public/Illustration/IlustracaoLogin.svg" alt="" />
+          <Typography variant='h3' color='white'>Revolucionando a Educacão</Typography>
+          <Typography variant='body2' color='white'>A IA que transforma seu jeito de aprender idiomas. Tornando-o mais interativo, adaptável, eficaz.</Typography>
+        </SwiperSlide>
+
+        <SwiperSlide className='swiper-slide-login'>
+          <img src="../../../public/Illustration/illustrationSlide2.svg" alt="" />
+          <Typography variant='h3' color='white'>Idiomas ao Seu Alcance</Typography>
+          <Typography variant='body2' color='white'>
+            Explore novos idiomas com suporte de IA. Personalizado para o seu aprendizado fluir naturalmente.</Typography>
+        </SwiperSlide>
+
+        <SwiperSlide className='swiper-slide-login'>
+          <img src="../../../public/Illustration/illustrationSlide3.svg" alt="" />
+          <Typography variant='h3' color='white'>Fluência Inteligente</Typography>
+          <Typography variant='body2' color='white'>
+            IA que entende seu ritmo. Aprenda idiomas com atividades que se adaptam a você.</Typography>
+        </SwiperSlide>
+
+      </Swiper>
+    </>
+  );
+}
