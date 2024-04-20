e222386d8c97a86fe36dfd6961451ba3324cbc17
juliaRufino <julia.rufino@sptech.school>
2024-04-20 17:48:02 -0300

    Add alt to images

--- src/components/SlidesLogin/SlidesLogin.tsx
+++ src/components/SlidesLogin/SlidesLogin.tsx
@@ -22,20 +22,20 @@ export default function SlideLogin() {
         className='mySwiper swiper-login'
       >
         <SwiperSlide className='swiper-slide-login'>
-          <img src='../../../public/Illustration/IlustracaoLogin.svg' alt='' />
+          <img src='../../../public/Illustration/IlustracaoLogin.svg' alt='Mulher tendo ideias junto com a IA' />
           <Typography variant='h3' color='white'>Revolucionando a Educacão</Typography>
           <Typography variant='body2' color='white'>A IA que transforma seu jeito de aprender idiomas. Tornando-o mais interativo, adaptável, eficaz.</Typography>
         </SwiperSlide>
 
         <SwiperSlide className='swiper-slide-login'>
-          <img src='../../../public/Illustration/illustrationSlide2.svg' alt='' />
+          <img src='../../../public/Illustration/illustrationSlide2.svg' alt='Mulher aprendendo um novo idioma através do computador' />
           <Typography variant='h3' color='white'>Idiomas ao Seu Alcance</Typography>
           <Typography variant='body2' color='white'>
             Explore novos idiomas com suporte de IA. Personalizado para o seu aprendizado fluir naturalmente.</Typography>
         </SwiperSlide>
 
         <SwiperSlide className='swiper-slide-login'>
-          <img src='../../../public/Illustration/illustrationSlide3.svg' alt='' />
+          <img src='../../../public/Illustration/illustrationSlide3.svg' alt='Mulher realizando testes online' />
           <Typography variant='h3' color='white'>Fluência Inteligente</Typography>
           <Typography variant='body2' color='white'>
             IA que entende seu ritmo. Aprenda idiomas com atividades que se adaptam a você.</Typography>
