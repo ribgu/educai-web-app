30b1adfc0b7941066b6beb03fd1ce18f108a144c
ribgu <gustavo.ribeiro@sptech.school>
2024-04-27 14:52:10 -0300

    altera client

--- src/lib/client/client.ts
+++ src/lib/client/client.ts
@@ -10,15 +10,15 @@ export default class Client {
   constructor(clientType: ClientType) {
     this.clientType = clientType
     this.axios = axios.create({
-      baseURL: this.clientType === 'ia-api' ? import.meta.env.API_URL_IA : import.meta.env.API_URL,
+      baseURL: this.clientType === 'ia-api' ? import.meta.env.VITE_API_URL_IA : import.meta.env.VITE_API_URL,
       withCredentials: true
     })
   }
 
   async login(
-    params: UserLogin
+    body: UserLogin 
   ) {
-    return (await this.axios.get('login', { params })).data
+    return (await this.axios.get('login', { data: body })).data
   }
 
   // outros métodos vcs devem criar um tipo na pasta types, copiem o UserLogin e alterem conforme a necessidade
