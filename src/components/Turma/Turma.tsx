8c6dede3c4155e3155fd04f6ddefb6298e96141c
ribgu <gustavo.ribeiro@sptech.school>
2024-04-30 19:48:40 -0300

    add edutar e deketar

--- src/components/Turma/Turma.tsx
+++ src/components/Turma/Turma.tsx
@@ -2,6 +2,9 @@ import Box from '@mui/material/Box/Box'
 import Typography from '@mui/material/Typography/Typography'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import IconButton from '@mui/material/IconButton/IconButton'
+import MenuItem from '@mui/material/MenuItem/MenuItem'
+import Menu from '@mui/material/Menu/Menu'
+import { useState } from 'react'
 
 type TurmaProps = {
     nome: string
@@ -11,9 +14,21 @@ type TurmaProps = {
 
 export default function Turma(props: TurmaProps) {
     const { nome, disciplina, qtdAlunos } = props
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const open = Boolean(anchorEl)
 
-    const handleClick = () => {
-        console.log('clicou')
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleClose = (editOrDelete: string) => {
+        setAnchorEl(null)
+
+        if (editOrDelete === 'edit') {
+            console.log('Editando')
+        } else if (editOrDelete === 'delete') {
+            console.log('Deletando')
+        }
     }
 
     return (
@@ -27,7 +42,7 @@ export default function Turma(props: TurmaProps) {
                 padding: '8px',
                 borderBottom: '2px solid #BEBEBE',
                 borderColor: '#BEBEBE'
-                }}>
+            }}>
                 <Box sx={{ display: 'flex', flexDirection: 'row', height: '100%', width: '60%', alignItems: 'center', gap: '10px' }}>
                     <img src='./logos/bookTwo.svg' alt='Ícone de livro' style={{ width: '26px', marginBottom: '5px' }} />
                     <Typography sx={{ fontSize: '16px', whiteSpace: 'nowrap' }}>{nome}</Typography>
@@ -35,6 +50,20 @@ export default function Turma(props: TurmaProps) {
                 <IconButton size='small' onClick={handleClick}>
                     <MoreVertIcon />
                 </IconButton>
+                <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={() => { handleClose('edit') }}>
+                        Editar
+                    </MenuItem>
+                    <MenuItem onClick={() => { handleClose('delete') }}>
+                        Apagar
+                    </MenuItem>
+                </Menu>
             </Box>
             <Box sx={{ width: '100%', height: '65%', padding: '8px', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
                 <Typography>Disciplina: {disciplina}</Typography>
