62632f5f16894b1d204315eeb7f3c6503c693052
ribgu <gustavo.ribeiro@sptech.school>
2024-05-02 20:52:24 -0300

    feat: Add audio recording functionality to TalkButton component

--- src/components/TalkButton/TalkButton.tsx
+++ src/components/TalkButton/TalkButton.tsx
@@ -1,40 +1,46 @@
 import { ReactMediaRecorder } from 'react-media-recorder'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Button from '@mui/material/Button'
 import MicIcon from '@mui/icons-material/Mic'
 import StopIcon from '@mui/icons-material/Stop'
 
 export default function TalkButton() {
   const [audio, setAudio] = useState<Blob | null>(null)
-  console.log(audio)
 
-  const handleSaveAudio = async (mediaBlobUrl: string) => {
-    console.log(mediaBlobUrl)
-    const response = await fetch(mediaBlobUrl)
-    const data = await response.blob()
-    setAudio(data)
-    console.log(data)
+  useEffect(() => {
+    if (audio) {
+      console.log('Audio state updated:', audio)
+    }
+  }, [audio])
+
+  const handleSaveAudio = async (mediaBlobUrl: string, status: string) => {
+    console.log(status)
+    if (mediaBlobUrl) {
+      const response = await fetch(mediaBlobUrl)
+      const data = await response.blob()
+      setAudio(data)
+      console.log('Audio saved:', data)
+    }
   }
-
   return (
     <ReactMediaRecorder
       audio
       render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
         <div>
+          {status}
           <Button
             sx={{ width: '24vw', padding: '16px', borderRadius: '10px' }}
             onClick={() => {
-              if (status === 'recording') {
-                console.log('stop')
-                stopRecording()
-                console.log(mediaBlobUrl)
-                if (mediaBlobUrl) {
-                  console.log('saving')
-                  handleSaveAudio(mediaBlobUrl)
-                }
-              } else {
-                console.log('start')
+              if (status === 'idle' || status === 'stopped') {
+                console.log(status, 'if')
                 startRecording()
+              } else {
+                console.log(status, 'else')
+                stopRecording()
+                console.log(status, 'else, after stop')
+                if (mediaBlobUrl) {
+                  handleSaveAudio(mediaBlobUrl, status)
+                }
               }
             }}
             color={status === 'recording' ? 'error' : 'primary'}
