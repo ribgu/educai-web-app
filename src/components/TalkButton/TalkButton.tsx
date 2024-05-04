a989d9f802d9b4745b5ebba845a870e728ed657c
ribgu <gustavo.ribeiro@sptech.school>
2024-05-04 00:04:58 -0300

    refactor: Update TalkButton component to use react-media-recorder hook

--- src/components/TalkButton/TalkButton.tsx
+++ src/components/TalkButton/TalkButton.tsx
@@ -1,61 +1,49 @@
-import { ReactMediaRecorder } from 'react-media-recorder'
+import { useReactMediaRecorder } from 'react-media-recorder'
 import { useState, useEffect } from 'react'
 import Button from '@mui/material/Button'
 import MicIcon from '@mui/icons-material/Mic'
 import StopIcon from '@mui/icons-material/Stop'
 
 export default function TalkButton() {
-  const [audio, setAudio] = useState<Blob | null>(null)
+  const [audio, setAudio] = useState<string | null>(null)
 
-  useEffect(() => {
-    if (audio) {
-      console.log('Audio state updated:', audio)
-    }
-  }, [audio])
+  const AudioRecorder = () => {
+    const {
+      status,
+      startRecording,
+      stopRecording,
+      mediaBlobUrl
+    } = useReactMediaRecorder({ audio: true })
 
-  const handleSaveAudio = async (mediaBlobUrl: string, status: string) => {
-    console.log(status)
-    if (mediaBlobUrl) {
-      const response = await fetch(mediaBlobUrl)
-      const data = await response.blob()
-      setAudio(data)
-      console.log('Audio saved:', data)
+    useEffect(() => {
+      if (status === 'stopped') {
+        setAudio(mediaBlobUrl ?? null)
+      }
+    }, [status, mediaBlobUrl])
+
+    const handleStartRecording = () => {
+      if (status === 'idle') {
+        startRecording()
+      }
+      if (status === 'recording') {
+        stopRecording()
+      }
+      if (status === 'stopped') {
+        setAudio(null)
+        startRecording()
+      }
     }
+
+    return (
+      <Button
+        sx={{ width: '24vw', padding: '16px', borderRadius: '10px' }}
+        onClick={status === 'recording' ? stopRecording : handleStartRecording}
+        color={status === 'recording' ? 'error' : 'primary'}
+        variant='contained'
+      >
+        {status === 'recording' ? <StopIcon /> : <MicIcon />}
+      </Button>
+    )
+
   }
-  return (
-    <ReactMediaRecorder
-      audio
-      render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-        <div>
-          {status}
-          <Button
-            sx={{ width: '24vw', padding: '16px', borderRadius: '10px' }}
-            onClick={() => {
-              if (status === 'idle' || status === 'stopped') {
-                console.log(status, 'if')
-                startRecording()
-              } else {
-                console.log(status, 'else')
-                stopRecording()
-                console.log(status, 'else, after stop')
-                if (mediaBlobUrl) {
-                  handleSaveAudio(mediaBlobUrl, status)
-                }
-              }
-            }}
-            color={status === 'recording' ? 'error' : 'primary'}
-            variant='contained'
-          >
-            {status === 'recording' ? <StopIcon /> : <MicIcon />}
-          </Button>
-          {audio && (
-            <audio
-              controls
-              src={URL.createObjectURL(audio)}
-            />
-          )}
-        </div>
-      )}
-    />
-  )
 }
