7f03b990d7739f518ee6b532da40bf13dec7578b
juliaRufino <julia.rufino@sptech.school>
2024-05-29 18:36:06 -0300

    Adaptando para o crud do post

--- src/components/PostsPage/PostsPage.tsx
+++ src/components/PostsPage/PostsPage.tsx
@@ -4,19 +4,83 @@ import Box from '@mui/material/Box/Box'
 import Button from '@mui/material/Button/Button'
 import Post from '../Post/Post'
 import { LoadingButton } from '@mui/lab'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { PostType } from '../../lib/types/Post'
+import useClient from '../../lib/client/useClient'
+import Typography from '@mui/material/Typography/Typography'
+import { ChangeEvent } from 'react'
 
 type postsPageProps = {
-    posts: {
-        dtPublicacao: Date
-        title: string
-    }[]
+    classroomId: string
 }
 
 export default function PostsPage(props: postsPageProps) {
-    const { posts } = props
+    const { classroomId } = props
 
+    const client = useClient()
+    const [posts, setPosts] = useState<PostType[]>([])  
+
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [file, setFile] = useState<File | null>(null)
+    const [datePosting, setDatePosting] = useState('')
+
+    const [modal, setModal] = useState<{ isLoading: boolean, isOpen: boolean, type: 'EDIT' | 'DELETE' | null }>({
+        isLoading: false,
+        isOpen: false,
+        type: null
+    })
+    
     const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [modalIsLoading, setModalIsLoading] = useState(false)
+
+    useEffect(() => {
+        setDatePosting(new Date().toISOString())
+    }, [])
+
+    // Atualizar os campos do modal
+    useEffect(() => {
+        if(modal.isOpen) {
+            setTitle(title)
+            setDescription(description)
+        }
+    }, [modal.isOpen])
+
+    // Carregar os posts
+    useEffect(() => {
+        if (classroomId) {
+          updatePosts()
+        }
+    }, [classroomId])
+
+    // Função de atualizar os posts
+    const updatePosts = () => {
+        if (classroomId) {
+          client.getPostsByClassroom(classroomId).then((data) => setPosts(data || []))
+        }
+    }
+
+    // Função de criar um post
+    const createPost = async (title: string, description: string, datePosting: string, file: File): Promise<void> => {
+        return await client.createPost({ title, description, datePosting, classroomId }, file).then(() => updatePosts())
+    }
+
+    const createAPost = () => {
+        if (title && description && file) {
+            setModalIsLoading(true)
+            createPost(title, description, datePosting, file).finally(() => {
+                setModalIsLoading(false)
+                setModalIsOpen(false)
+            })
+        }
+    }
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setFile(e.target.files[0])
+        }
+    }
+
     return (
         <>
             <Modal
@@ -28,9 +92,21 @@ export default function PostsPage(props: postsPageProps) {
                 onClose={() => setModalIsOpen(false)}
                 onOpen={() => setModalIsOpen(true)}
             >
-                <TextField id='outlined-basic' variant='outlined' label='Título*' />
-                <TextField id='outlined-basic' variant='outlined' label='Descrição*' />
-                <TextField id='outlined-basic' variant='outlined' label='Upload de arquivo' />
+                <TextField 
+                id='outlined-basic' 
+                variant='outlined' 
+                label='Título*' 
+                onChange={(e) => setTitle(e.target.value)}/>
+                
+                <TextField id='outlined-basic'
+                 variant='outlined'
+                label='Descrição*'
+                onChange={(e) => setDescription(e.target.value)}
+                 />
+
+                <TextField type='file'
+                 onChange={handleFileChange}/>
+
                 <Box sx={{
                     display: 'flex',
                     justifyContent: 'space-between',
@@ -54,14 +130,18 @@ export default function PostsPage(props: postsPageProps) {
                         },
                         paddingY: '12px',
                         width: '48%'
-                    }} variant='contained'>Criar turma</LoadingButton>
+                    }} variant='contained' onClick={createAPost} loading={modalIsLoading}>Criar Post</LoadingButton>
                 </Box>
             </Modal>
 
-            <Box sx={{ display: 'flex', gap: '16px', flexDirection: 'column' }}>
-                {posts.map((post, index) => (
-                    <Post key={index} dtPublicacao={post.dtPublicacao} title={post.title} />
-                ))}
+            <Box sx={{ display: 'flex', gap: '16px', flexDirection: 'column', overflow: 'auto'}}>
+                {Array.isArray(posts) && posts.length > 0 ? (
+                    posts.map((post, index) => (
+                        <Post key={index} id={post.id} datePosting={post.datePosting} title={post.title} description={post.description} url={post.url} updatePost={updatePosts} />
+                    ))
+                ) : (
+                    <Typography variant="h6" align="center">Nenhum post...</Typography>
+                )}
             </Box>
         </>
     )
