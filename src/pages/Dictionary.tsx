5993abf042a0eb13a7144526549746af89e125b9
ribgu <gustavo.ribeiro@sptech.school>
2024-05-29 18:41:00 -0300

    add button to open modal

---
+++ src/pages/Dictionary.tsx
@@ -0,0 +1,10 @@
+import IconButton from '@mui/material/IconButton/IconButton'
+import BookIcon from '@mui/icons-material/Book'
+
+export default function Dictionary() {
+    return (
+        <IconButton size='small'>
+            <BookIcon />
+        </IconButton>
+    )
+}
