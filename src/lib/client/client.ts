7956335b1ca949b3904149d36f11fa4309bf8716
juliaRufino <julia.rufino@sptech.school>
2024-06-02 16:16:36 -0300

    Adicionando função de salvar a resposta

--- src/lib/client/client.ts
+++ src/lib/client/client.ts
@@ -6,6 +6,7 @@ import { LeaderboardType } from '../types/Leaderboard'
 import { PostType } from '../types/Post'
 import { classWork } from '../types/ClassWork'
 import { DictonaryResponse } from '../types/DictonaryResponse'
+import { AnswerType } from '../types/Answer'
 
 type ClientProps = {
   clientType: 'ia-api' | 'api',
@@ -24,7 +25,7 @@ export default class Client {
     })
 
     this.axios.interceptors.request.use((config) => {
-      if(!config.url?.includes('user/auth') && !config.url?.includes('user/refreshToken')) {
+      if (!config.url?.includes('user/auth') && !config.url?.includes('user/refreshToken')) {
         config.headers.Authorization = this.clientProps.clientToken
       }
       return config
@@ -38,7 +39,7 @@ export default class Client {
         const originalRequest = error.config
 
         console.log(error)
-        if(error.response.status === 500 && !originalRequest._retry && error.response.data.message.includes('Token has expired')) {
+        if (error.response.status === 500 && !originalRequest._retry && error.response.data.message.includes('Token has expired')) {
           originalRequest._retry = true
 
           return this.refreshToken().then(response => {
@@ -59,7 +60,7 @@ export default class Client {
 
   async login(
     body: UserLogin
-  ): Promise<{token: string}> {
+  ): Promise<{ token: string }> {
     return (await this.axios.post('user/auth', body)).data
   }
 
@@ -67,7 +68,7 @@ export default class Client {
     return (await this.axios.get('/user/classrooms')).data
   }
 
-  async createClassroom(body: {title: string, course: string}): Promise<void> {
+  async createClassroom(body: { title: string, course: string }): Promise<void> {
     return (await this.axios.post('/classroom', body))
   }
 
@@ -79,7 +80,7 @@ export default class Client {
     return (await this.axios.delete(`/classroom/${classroomId}`))
   }
 
-  async updateClassroom(classroomId: string, body: {title?: string, course?: string}): Promise<void> {
+  async updateClassroom(classroomId: string, body: { title?: string, course?: string }): Promise<void> {
     return (await this.axios.patch(`/classroom/${classroomId}`, body))
   }
 
@@ -94,12 +95,12 @@ export default class Client {
     formData.append('datePosting', body.datePosting)
     formData.append('classroomId', body.classroomId)
     formData.append('file', file)
-    
+
     const response = await this.axios.post('/posts', formData)
     const post: PostType = response.data
     const url = await this.getUrlArquivoPost(post.id)
     return { post, url }
-}
+  }
 
   async getUrlArquivoPost(postId: string): Promise<string> {
     return (await this.axios.get(`/posts/${postId}/download`)).data
@@ -109,12 +110,25 @@ export default class Client {
     return (await this.axios.delete(`/posts/${postId}`))
   }
 
-  async updatePost(postId: string, body: {title?: string, description?: string}): Promise<void> {
+  async updatePost(postId: string, body: { title?: string, description?: string }): Promise<void> {
     return (await this.axios.patch(`/posts/${postId}`, body))
   }
 
   async getPostsByClassroom(classroomId: string): Promise<PostType[]> {
     return (await this.axios.get(`/classroom/${classroomId}/posts`)).data
+  }
+
+  async getClassworkById(classworkId: string): Promise<classWork> {
+    return (await this.axios.get(`/classwork/${classworkId}`)).data
+  }
+
+  async addAnswer(
+    body: AnswerType,
+    headers: {
+      classworkId: string,
+      userId: string
+    }): Promise<void> {
+    return await this.axios.post('/answer', body, { headers })
   }
 
   async refreshToken() {
@@ -133,8 +147,8 @@ export default class Client {
 
   async getResponse(
     question: string
-  ): Promise<EduResponse>{
-    const request = await this.axios.post('edu-response', { question } )
+  ): Promise<EduResponse> {
+    const request = await this.axios.post('edu-response', { question })
     console.log(request)
     return request.data
   }
@@ -159,14 +173,14 @@ export default class Client {
     return (await this.axios.post('/classwork', classWork, { headers }))
 
   }
-  
-  async generateEducationalMaterial(payload: {youtubeLink?: string, audio?: File | null, document?: File | null}): Promise<AxiosResponse<ArrayBuffer>> {
+
+  async generateEducationalMaterial(payload: { youtubeLink?: string, audio?: File | null, document?: File | null }): Promise<AxiosResponse<ArrayBuffer>> {
     const formData = new FormData()
 
     payload.youtubeLink && formData.append('youtubeLink', payload.youtubeLink)
     payload.audio && formData.append('audio', payload.audio)
-    payload.document &&  formData.append('document', payload.document)
-  
+    payload.document && formData.append('document', payload.document)
+
     return (await this.axios.post('/generate-educational-resource', formData, { responseType: 'arraybuffer' }))
   }
 
