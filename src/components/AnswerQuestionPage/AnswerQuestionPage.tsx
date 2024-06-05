587045370b09fbe435b8ec95b247d64ce30304fb
ribgu <gustavo.ribeiro@sptech.school>
2024-06-04 21:26:22 -0300

    refactor pai

--- src/components/AnswerQuestionPage/AnswerQuestionPage.tsx
+++ src/components/AnswerQuestionPage/AnswerQuestionPage.tsx
@@ -2,7 +2,7 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import AnswerQuestion from '../AnswerQuestion/AnswerQuestion'
 import Button from '@mui/material/Button'
-import { ClassworkType } from '../../lib/types/Classwork'
+import { Classwork } from '../../lib/types/ClassWork'
 import { useState, useEffect } from 'react'
 import useClient from '../../lib/client/useClient'
 
@@ -10,15 +10,55 @@ type AnswerQuestionPageProps = {
   classworkId: string
 }
 
+type SendAnswerData = {
+  datePosting: string
+  questionAnswers: QuestionAnswers[]
+}
+
+type QuestionAnswers = {
+  optionKey: string
+  questionId: string
+}
+
 export default function AnswerQuestionPage(props: AnswerQuestionPageProps) {
   const { classworkId } = props
-  const [classwork, setClasswork] = useState<ClassworkType>()
+  const [classwork, setClasswork] = useState<Classwork>()
+  const [answers, setAnswers] = useState<QuestionAnswers[]>([])
+  const [completedQuestion, setCompletedQuestion] = useState<SendAnswerData>()
   const client = useClient()
+
+  const handleSelectAlternative = (questionId: string, answerKey: string) => {
+    setAnswers(prevAnswers => {
+      const index = prevAnswers.findIndex(answer => answer.questionId === questionId)
+      const newAnswer = { questionId, optionKey: answerKey }
+
+      if (index === -1) {
+        return [...prevAnswers, newAnswer]
+      } else {
+        return prevAnswers.map((answer, i) => i === index ? newAnswer : answer)
+      }
+    })
+  }
+
+  useEffect(() => {
+    console.log('answers updated:', answers)
+  }, [answers])
+
+  useEffect(() => {
+    if (completedQuestion) {
+      console.log('completedQuestion:', completedQuestion)
+    }
+  }, [completedQuestion])
+
+  const handleSendAnswers = () => {
+    const datePosting = new Date().toISOString()
+    setCompletedQuestion({ datePosting, questionAnswers: answers })
+  }
 
   useEffect(() => {
     client.getClassworkById(classworkId).then((res) => setClasswork(res))
-  }
-  , [classworkId])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [classworkId])
 
   return (
     <Box>
@@ -34,9 +74,17 @@ export default function AnswerQuestionPage(props: AnswerQuestionPageProps) {
         overflowY: 'auto',
       }}>
         {classwork?.questions.map((question, index) => (
-          <AnswerQuestion key={index} question={question} />
+          <AnswerQuestion key={index}
+            description={question.description}
+            options={question.options}
+            id={question.id}
+            handleSelectAlternative={handleSelectAlternative}
+          />
         ))}
-        <Button variant='contained'>Finalizar</Button>
+        <Button
+          variant='contained'
+          onClick={handleSendAnswers}
+        >Finalizar</Button>
       </Box>
     </Box>
   )
